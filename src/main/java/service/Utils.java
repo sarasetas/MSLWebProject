6b1277a24dd2bd131ea231e51d00c649b0cb7b18
Ssetas <sarasetas@live.com>
2017-09-18 17:36:10 +0100

    utils

---
+++ src/main/java/service/Utils.java
@@ -0,0 +1,11 @@
+package service;
+
+public class Utils {
+
+	public Utils() {
+		// TODO Auto-generated constructor stub
+	}
+
+	// error messages
+	public static final String RECEIPT_ALREADY_EXISTS = "O recibo introduzido já existe no sistema";
+}
